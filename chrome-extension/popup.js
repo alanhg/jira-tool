8d2e84d5141d19cc12c90db87ad84133a4578dd4
Alan He <qianghe421@gmail.com>
2021-08-01 12:40:27 +0800

    update manifest to v3

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,15 +2,27 @@ const extensionVersion = chrome.runtime.getManifest().version;
 document.getElementById('jVersion').innerText = extensionVersion;
 
 const localSetting = {
-    disableQuickToEditIsOn: false
+    disableQuickToEditIsOn: true,
+    // 自动选中
+    autoSelect: true
 }
 const disabledQuickToEditEl = document.getElementById('disabledQuickToEdit');
+const autoSelectToEditEl = document.getElementById('autoSelect');
 
 readDisableQuickToEdit().then((res) => {
     localSetting.disableQuickToEditIsOn = res;
     disabledQuickToEditEl.checked = res;
 })
 
+readAutoSelect().then((res) => {
+    localSetting.autoSelect = res;
+    autoSelectToEditEl.checked = res;
+})
+
 disabledQuickToEditEl.addEventListener('change', async (e) => {
     setDisableQuickToEdit(e.target.checked);
+})
+
+autoSelectToEditEl.addEventListener('change', async (e) => {
+    setAutoSelect(e.target.checked);
 })
--- chrome-extension/storage.js
+++ chrome-extension/storage.js
@@ -1,4 +1,5 @@
 const DISABLE_QUICK_TO_EDIT_KEY = 'DISABLE_QUICKTO_EDIT_SWITCH';
+const AUTO_SELECT_KEY = 'AUTO_SELECT_KEY';
 
 function readStorage(key) {
     return new Promise(function (resolve) {
@@ -19,9 +20,17 @@ function writeStorage(key, value) {
 }
 
 async function readDisableQuickToEdit() {
-    return await readStorage(DISABLE_QUICK_TO_EDIT_KEY) === 'true'
+    return (await readStorage(DISABLE_QUICK_TO_EDIT_KEY) === 'true') || localSetting.disableQuickToEditIsOn
 }
 
 async function setDisableQuickToEdit(value) {
     return await writeStorage(DISABLE_QUICK_TO_EDIT_KEY, String(value))
+}
+
+async function readAutoSelect() {
+    return await readStorage(AUTO_SELECT_KEY) === 'true' || localSetting.autoSelect
+}
+
+async function setAutoSelect(value) {
+    return await writeStorage(AUTO_SELECT_KEY, String(value))
 }
